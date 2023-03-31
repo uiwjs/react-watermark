6c1b336a0a4498a50a5250d27091fc0c559c6a6f
jaywcjlove <398188662@qq.com>
2023-03-31 17:40:31 +0800

    chore(deps): update dependency tsbb to v4 #15
    https://github.com/jaywcjlove/tsbb/issues/439

--- core/dist/watermark.js
+++ core/dist/watermark.js
@@ -2,7 +2,7 @@
  * @uiw/watermark.js v0.0.5 
  * JavaScript library for generating image watermarks using canvas. 
  * 
- * Copyright (c) 2022 kenny wang <wowohoo@qq.com> 
+ * Copyright (c) 2023 kenny wang <wowohoo@qq.com> 
  * https://uiwjs.github.io/react-watermark 
  * Licensed under the MIT license 
  */
